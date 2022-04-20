6902bbc2da3088427f51f7179100fb086eb78913
yearliny <yearliny@outlook.com>
2022-04-20 16:46:32 +0800

    更新版本喝配置

--- src/main/java/ga/yuan/yogo/Application.java
+++ src/main/java/ga/yuan/yogo/Application.java
@@ -1,6 +1,7 @@
 package ga.yuan.yogo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +15,8 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // 获取应用用户变量 server.port 值
         String serverPort = context.getEnvironment().getProperty("server.port");
         log.info("Yogo start at http://localhost:{}!", serverPort);
     }
