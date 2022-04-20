27ebb759a53711a4d757ea7a836d9879eeb756cd
yearliny <yearliny@outlook.com>
2022-04-20 18:20:56 +0800

    更新应用

--- src/main/java/ga/yuan/yogo/Application.java
+++ src/main/java/ga/yuan/yogo/Application.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -17,8 +18,8 @@ public class Application {
         ApplicationContext context = SpringApplication.run(Application.class, args);
 
         // 获取应用用户变量 server.port 值
-        String serverPort = context.getEnvironment().getProperty("server.port");
-        log.info("Yogo start at http://localhost:{}!", serverPort);
+        ServerProperties serverProperties = context.getBean(ServerProperties.class);
+        log.info("Yogo start at http://localhost:{}!", serverProperties.getPort());
     }
 
 }
